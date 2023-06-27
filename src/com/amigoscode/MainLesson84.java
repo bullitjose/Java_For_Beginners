2586f7ae64e7ba00cb222aaef4082d2292cb3313
JOSE <bullit.jose@gmail.com>
2023-06-27 19:38:11 +0200

    Actualizar codigo 0627

--- src/com/amigoscode/MainLesson84.java
+++ src/com/amigoscode/MainLesson84.java
@@ -3,10 +3,12 @@ package com.amigoscode;
 import java.util.Arrays;
 
 public class MainLesson84 {
+
+    private String brand="amigosCode";//belong to instace of Class, dont use var
     public static void main(String[] args){
      //Call terminal java and run: $ com.amigoscode.MainLesson84 hello ciao
      //Arguments:
-
+        //lesson 85
        if(args.length>0){
            System.out.println("Arguments: ");
            System.out.println(Arrays.toString(args));
@@ -16,6 +18,38 @@ public class MainLesson84 {
        }
 
 
-    }
+       //Lesson 86, Type inference with var keyword
+        //only for local variables!!
+       var name="Jamila";
+       var names=new String[]{"Andre","Jamila","Ali","Jones","Matt"};
+       var age=12;
+       var isAdult=false;
+       var balance=1_000_000.33;
+
+//lesson 87 Break and Continue
+        var numbers=new int[]{1,2,3,4,5};
+        for(int number:numbers){
+            if(number==3) {
+                break;
+            }
+            System.out.println(number);
+        }
+        //Lesson 88 and 89
+        for(String name1:names){
+            if(name1.startsWith("A")){
+                //continue;//move to next iteration of names
+
+                //break; //break completly de for iteration
+
+                //lesson 89
+                return;// if the method is void (public static void main(String[] args){)
+                        //return exit the method!!, in this case main
+
+            }
+            System.out.println(name1);
+        }
+
+
+}
 
 }
